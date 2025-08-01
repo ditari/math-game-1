a0576236d4268dfd099dd4e5b3f425118ae4e4bf
ditari <xiratid@gmail.com>
2025-08-01 08:41:09 +0700

    fix bossroom scale to hp

--- scripts/bossroomlv1.gd
+++ scripts/bossroomlv1.gd
@@ -2,6 +2,8 @@ extends Control
 
 #var bgwallscene: PackedScene = load("res://scenes/bgwall.tscn")
 #var bgwall
+var ygaps
+var xgaps
 
 var bossscene: PackedScene = load("res://scenes/bosslv1.tscn")
 var boss
@@ -10,9 +12,10 @@ func _ready():
 	#bgwall = bgwallscene.instantiate()
 	#bgwall.position = Vector2(0,128)
 	#$array.add_child(bgwall)
+	update_sprite()
 	
 	$playerprogressbar.value = Global.playerhp	
-	$scorelabel.text = "Score: " +str (Global.score)
+	$scorelabel.text = "SCORE: " +str (Global.score)
 
 	if Global.items[1] > 0:
 		$key1.get_node("AnimatedSprite2D").play("on")	
@@ -31,16 +34,53 @@ func _ready():
 
 	#boss
 	boss = bossscene.instantiate()
-	boss.position = Vector2(264,600)
+	boss.position = Vector2(xgaps-32,5*ygaps)
 	boss.scale = Vector2(1.5,1.5)
 	
 	boss.connect("button_pressed", _on_button_pressed)
 	
 	$array.add_child(boss)
 
-func _on_button_pressed():
-	get_tree().change_scene_to_file("res://scenes/bossfightlv1.tscn") 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func update_sprite():
+	var screen_size = get_viewport_rect().size
+	ygaps = screen_size.y/10
+	xgaps = (screen_size.x - 128)/2
+	
+	$Bgwall.position.x = 360
+	$Bgwall.position.y = ygaps + 192
+	
+	$scorelabel.position.y = 0.3 * ygaps
+	
+	$hplabel.position.x = 32
+	$hplabel.position.y = 8*ygaps + 60
+	
+	$playerprogressbar.position.x = 192
+	$playerprogressbar.position.y = 8*ygaps + 72
+	
+	$itemlabel.position.x = 32
+	$itemlabel.position.y = 9*ygaps + 16
+	
+	$key1.position.x = 192
+	$key1.position.y = 9*ygaps
+
+	$key1label.position.x =256
+	$key1label.position.y =9*ygaps
+	
+	$key2.position.x = 368 #384
+	$key2.position.y = 9*ygaps	
+	
+	$key2label.position.x =432 #448
+	$key2label.position.y =9*ygaps	
+	
+	$calculator.position.x = 544
+	$calculator.position.y = 9*ygaps
+	
+	$calculatorlabel.position.x = 608
+	$calculatorlabel.position.y = 9*ygaps	
+
+func _on_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/bossfightlv1.tscn") 
